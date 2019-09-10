2d843438d064f7caf16199d2eb4be1e54bac226c
Matheus Barth <matheusbarthf@gmail.com>
2019-09-10 11:27:33 -0300

    Aula 3 - Arrays, Loop e Estilos

--- js/principal.js
+++ js/principal.js
@@ -1,30 +1,40 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
-var tdPeso = paciente.querySelector(".info-peso");
-var tdAltura = paciente.querySelector(".info-altura");
-var tdImc = paciente.querySelector(".info-imc");
+var pacientes = document.querySelectorAll(".paciente");
 
-var peso = tdPeso.textContent;
-var altura = tdAltura.textContent;
+for (var i = 0; i < pacientes.length; i++) {
+    
+    var paciente = pacientes[i];
+    var tdPeso = paciente.querySelector(".info-peso");
+    var tdAltura = paciente.querySelector(".info-altura");
+    var tdImc = paciente.querySelector(".info-imc");
+    var peso = tdPeso.textContent;
+    var altura = tdAltura.textContent;
+    var pesoEhValido = true;
+    var alturaEhValido = true;
 
-var pesoEhValido = true;
-var alturaEhValido = true;
+    if (peso <= 0 || peso > 100) {
+        console.log("Peso Inválido");
+        tdImc.textContent = "Peso Inválido";
+        pesoEhValido = false;
 
-if (peso <= 0 || peso > 100) {
-    console.log("Peso Inválido");
-    tdImc.textContent = "Peso Inválido";
-    pesoEhValido = false;
+        paciente.classList.add("paciente-invalido");
+    };
+
+    if (altura <=0 || altura > 3) {
+        console.log("Altura Inválida");
+        tdImc.textContent = "Altura Inválida";
+        alturaEhValido = false;
+
+        paciente.classList.add("paciente-invalido");
+    };
+
+    if (pesoEhValido && alturaEhValido) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
+
 };
 
-if (altura <=0 || altura > 3) {
-    console.log("Altura Inválida");
-    tdImc.textContent = "Altura Inválida";
-    alturaEhValido = false;
-};
 
-if (pesoEhValido && alturaEhValido) {
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc;
-}
